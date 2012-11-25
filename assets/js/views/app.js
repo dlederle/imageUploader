38ffbc6b9dfe6013570310a9da03fa38c5ad0269
Dylan Lederle-Ensign <dylan.lederle.ensign@gmail.com>
2012-11-24 21:41:47 -0500

    Initial commit

---
+++ assets/js/views/app.js
@@ -0,0 +1,20 @@
+var app = app || {};
+
+$(function($) {
+  'use strict';
+
+  app.AppView = Backbone.View.extend({
+    el: '.container',
+
+    events: {
+    },
+
+    initialize: function() {
+    },
+
+    render: function() {
+      return this;
+    }
+
+  });
+});
